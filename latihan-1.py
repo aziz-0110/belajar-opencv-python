a9622859399e56a2739ea9fd402f2cc8b1b45103
aziz-0110 <222degree@gmail.com>
2024-03-26 13:13:31 +0700

    pengenalan opencv

---
+++ latihan-1.py
@@ -0,0 +1,21 @@
+import cv2
+
+def showImg(imgPath):
+    # cv2.IMREAD_COLOR == 1 => tidak membawa informasi apapun
+    img = cv2.imread(imgPath, cv2.IMREAD_COLOR)
+    img = cv2.imread(imgPath, 1)
+
+    # cv2.IMREAD_GRAYSCALE == 0 => gambar di konvert ke skala abu""
+    # img = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE)
+    # img = cv2.imread(imgPath, 0)
+
+    # cv2.IMREAD_UNCHANGED == -1 => gambar di muat dengan skala apa adanya
+    # img = cv2.imread(imgPath, cv2.IMREAD_UNCHANGED)
+    # img = cv2.imread(imgPath, -1)
+
+
+    cv2.imshow("pertemuan 1", img)
+    cv2.waitKey(0)
+
+
+showImg("./img_crop_1_1.png")
